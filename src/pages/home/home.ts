a0cc91ff33cdfecd930060c0753eb43902aca627
Teerasej <teerasej@gmail.com>
2017-09-20 12:07:19 +0700

    Add logic to calculate discounted price

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  message;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  calculate(price, discount){
+
+    if(price != undefined && discount != undefined)
+    {
+      let result = (price * (100 - discount))/100;
+      this.message = result + ' บาท';
+    }
+    else 
+    {
+      this.message = "ใส่ราคา และส่วนลดให้ครบด้วยค่ะ";
+    }
 
   }
 
